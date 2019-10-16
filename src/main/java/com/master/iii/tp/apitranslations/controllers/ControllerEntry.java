e424e4e9da3e0c1724dc2a77d89f54bb3b44ddd0
Miguel Yann <tsambamiguelito@gmail.com>
2019-10-16 03:42:29 +0200

    Fix URL modify and test 2 for post traduction Ok, add new update collection json

--- src/main/java/com/master/iii/tp/apitranslations/controllers/ControllerEntry.java
+++ src/main/java/com/master/iii/tp/apitranslations/controllers/ControllerEntry.java
@@ -4,7 +4,6 @@ package com.master.iii.tp.apitranslations.controllers;
 import com.master.iii.tp.apitranslations.entities.Entry;
 import com.master.iii.tp.apitranslations.services.entry.services.ServiceEntry;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.Optional;
 
 @Controller
-@RequestMapping("/applications/{id}/transaltions/{id}/entries")
+@RequestMapping("/applications/{idApplication}/translations/{idTranslations}/entries")
 public class ControllerEntry {
     @Autowired
     ServiceEntry serviceEntry;
@@ -44,7 +43,7 @@ public class ControllerEntry {
     public ResponseEntity deleteEntry(@PathVariable Integer id){
 
         serviceEntry.deleteEntry(id);
-        return  ResponseEntity.status(200).body("Entry " + id + "est bien supprimeé");
+        return ResponseEntity.status(200).body("Entry " + id + " est bien supprimeé");
 
     }
 }
--- src/main/java/com/master/iii/tp/apitranslations/controllers/ControllerTranslations.java
+++ src/main/java/com/master/iii/tp/apitranslations/controllers/ControllerTranslations.java
@@ -1,15 +1,15 @@
 package com.master.iii.tp.apitranslations.controllers;
 
 
-
 import com.master.iii.tp.apitranslations.entities.Translations;
-
 import com.master.iii.tp.apitranslations.services.translations.services.ServiceTranslation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
 
@@ -21,7 +21,7 @@ public class ControllerTranslations {
     ServiceTranslation serviceTranslation;
 
 
-    @PostMapping(path = "{id}/transaltions")
+    @PostMapping(path = "{id}/translations")
    public  ResponseEntity createTranslation( @RequestBody @Valid Translations translations){
 
         return new ResponseEntity<>(serviceTranslation.createTranslation(translations), HttpStatus.CREATED);
